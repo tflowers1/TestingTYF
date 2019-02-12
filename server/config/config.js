381384388e02e514bc942cf0fb11b3ecb18b12b3
Teresa F <46470615+tflowers1@users.noreply.github.com>
2019-02-12 00:06:12 -0500

    Add files via upload

---
+++ server/config/config.js
@@ -0,0 +1,11 @@
+//This file holds any configuration variables we may need 
+//'config.js' is typically ignored by git to protect sensitive information, such as your database's username and password
+
+module.exports = {
+  db: {
+    uri: 'mongodb://tflowers1:Turtle1@ds161804.mlab.com:61804/tyfadmin', //place the URI of your mongo database here.
+  }, 
+  port: 8080
+//process.env.PORT || "8080"
+  // for heroku
+};
